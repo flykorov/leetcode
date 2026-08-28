5fb811927fbeec22fd08055de0e055875e9e00b8
flykorov <flykorov@gmail.com>
2026-08-28 13:10:43 +0200

    169

---
+++ problem_solved/easy/169.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        c = 0
+        m = 0
+
+        for i in nums:
+            if c == 0:
+                m = i
+                c += 1
+            elif m == i:
+                c += 1
+            else:
+                c -= 1
+
+        return m
